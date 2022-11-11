651e6185d7243d5a186bb288fec62ad28f8277c7
Andra Muntean <andramuntean@andras-air.home>
2022-11-11 09:28:42 +0200

    added two subclasses dog and cat

--- Animal.cpp
+++ Animal.cpp
@@ -4,26 +4,75 @@ using namespace std;
 class Animal
 {
     public:
-    string name;
-    string breed;
-    string size;
-    string color;
-    int age;
+        string name;
+        string breed;
+        string size;
+        string color;
+        int age;
 
-    void printInfo()
-    {
-        cout<<"Name is "<<name<<endl;
-        cout<<"Age is "<<age<<endl;
-        cout<<"Breed is "<<breed<<endl;
-        cout<<"Size is "<<breed<<endl;
-        cout<<"Color is "<<breed<<endl;
-    }
+    public:
+        void printInfo()
+        {
+            cout<<"Animal's"<<endl;
+            cout<<"Name is "<<name<<endl;
+            cout<<"Age in human years is "<<age<<endl;
+            cout<<"Breed is "<<breed<<endl;
+            cout<<"Size is "<<size<<endl;
+            cout<<"Color is "<<color<<endl;
+    
+        }
+        int calculateAge()
+        {
+            return age * 1;
+        }
     Animal();
     ~Animal();
     void putAnimal(string name, string breed, string color, string size, int age);
     string getAnimal();
 };
 
+class Dog : public Animal
+{
+    public:
+        void printInfo()
+        {
+            cout<<"Dog's"<<endl;
+            cout<<"Name is "<<name<<endl;
+            cout<<"Age in human years is "<<age<<endl;
+            cout<<"Breed is "<<breed<<endl;
+            cout<<"Size is "<<size<<endl;
+            cout<<"Color is "<<color<<endl;
+        }
+        int calculateAge()
+        {
+            return age * 7;
+        }
+
+};
+
+class Cat : public Animal
+{
+    public:
+        void printInfo()
+        {
+            cout<<"Cat's"<<endl;
+            cout<<"Name is "<<name<<endl;
+            cout<<"Age in human years is "<<age<<endl;
+            cout<<"Breed is "<<breed<<endl;
+            cout<<"Size is "<<size<<endl;
+            cout<<"Color is "<<color<<endl;
+        }
+        int calculateAge()
+        {
+            if(age > 6)
+                return (age - 6) * 4 + 40;
+            else
+                return (age * 19) / 3 + 1; 
+        }
+
+};
+
+//constructor
 Animal::Animal()
 {
     name=" ";
@@ -33,18 +82,19 @@ Animal::Animal()
     age=0;
     cout<<"animal initialized.\n";
 }
+//destructor
 Animal::~Animal()
 {
-    cout<<"animal destroyed(destructered)./n";
+    cout<<"animal destroyed(destructered).\n";
 }
 
-void Animal::putAnimal(string name, string breed, string color, string size, int age)
+void Animal::putAnimal(string n, string b, string c, string s, int a)
 {
-    name=name;
-    breed=breed;
-    color=color;
-    size=size;
-    age=age;
+    name=n;
+    breed=b;
+    color=c;
+    size=s;
+    age=a;
 }
 
 string Animal::getAnimal()
@@ -54,16 +104,38 @@ string Animal::getAnimal()
 
 int main()
 {
-    Animal animal, animal1;
-    animal.putAnimal("Caine","bichon","negru","big",2);
-    animal1.name = "Caine";
-    animal1.breed = "bichon";
-    animal1.size = "big";
-    animal1.age = 2;
-    animal1.color = "negru";
-    animal1.printInfo();
-    animal.getAnimal();
+    Animal animal;
+    Dog dog;
+    Cat cat;
+    //animal.putAnimal("Caine","bichon","negru","big",2);
+    animal.name = "domestic animal";
+    animal.breed = "mixt";
+    animal.size = "big";
+    animal.age = 4;
+    animal.color = "black";
 
+    dog.name = "Rex";
+    dog.breed = "labrador";
+    dog.size = "big";
+    dog.age = 2;
+    dog.color = "white";
+
+    cat.name= "Kitty";
+    cat.breed= "american shorthair";
+    cat.size= "medium";
+    cat.age= 3;
+    cat.color= "grey";
+
+    animal.printInfo();
+    cout<<"Animal's age "<<animal.calculateAge();
+    cout<<endl<<endl;
+    dog.printInfo();
+    cout<<"Dog's age "<<dog.calculateAge();
+    cout<<endl<<endl;
+    cat.printInfo();
+    cout<<"Cat's age "<<cat.calculateAge();
+    cout<<endl;
+    //animal.getAnimal();
 
     return 0;
 
